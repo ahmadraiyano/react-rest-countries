aa60bd067bc8919690a1c687470db30ac7e99d8b
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-02-26 23:38:39 +0600

    toggled btn with useState

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,21 +1,24 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
+import React, { use } from "react";
+import Country from "../Country/Country";
+import './Countries.css'
 
-const Countries = ({loadCountries}) => {
-    const countriesData = use(loadCountries)
-    const forbidden = countriesData.countries
+const Countries = ({ loadCountries }) => {
+  const countriesData = use(loadCountries);
+  const forbidden = countriesData.countries;
 
-    const idToRemove = "Israel";
-    const countries = forbidden.filter(obj => obj.name.common !== idToRemove);
-    
-    return (
-        <div>
-            <h2>In the countries: {countries.length}</h2>
-            {
-                countries.map(country=> <Country key={country.ccn3.ccn3} country={country}></Country>)
-            }
-        </div>
-    );
+  const idToRemove = "Israel";
+  const countries = forbidden.filter((obj) => obj.name.common !== idToRemove);
+
+  return (
+    <div>
+      <h2>In the countries: {countries.length}</h2>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.ccn3.ccn3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
+    const [visited, setVisited] = useState(false)
+    const handleVisited = () => {
+        // setVisited(true)
+
+
+        //1. toggle visited-not visited
+        // if(visited){
+            //     setVisited(false)
+            // } else {
+                //     setVisited(true)
+                // }
+                
+                
+        //2. toggle visited-not visited
+        // setVisited(visited? false: true)
+
+        
+        //3. toggle visited-not visited
+        setVisited(!visited)
+    }
     
     return (
-        <div>
+        <div className={`country ${visited&& 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name: {country.name.common}</h2>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 250000 ? "Big Country" : "Small Country"}</p>
+            <p>{}</p>
+            <button className={visited&& "btn-visited"} onClick={handleVisited}>{visited? "Visited": "Not Visited"}</button>
         </div>
     );}
 
